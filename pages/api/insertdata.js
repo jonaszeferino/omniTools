05150346c5bb9f57356014b149c42e7c986506ce
jonaszeferino <75401113+jonaszeferino@users.noreply.github.com>
2023-03-26 03:15:05 -0300

    two methods working get and post with select and insert values, and connection in new component(page)

---
+++ pages/api/insertdata.js
@@ -0,0 +1,21 @@
+import mysql from "mysql2/promise";
+import { getConnection } from "../connectiondb";
+
+export default async function handler(req, res) {
+  const connection = await getConnection();
+
+  try {
+    const { username, password, email, full_name } = req.body;
+
+    const query =
+      "INSERT INTO users (username, password, email, full_name) VALUES (?, ?, ?, ?)";
+    const values = [username, password, email, full_name];
+
+    const [result] = await connection.execute(query, values);
+    connection.end();
+
+    res.status(200).json({ message: "Dados inseridos com sucesso." });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
---
+++ pages/connectiondb.js
@@ -0,0 +1,16 @@
+import mysql from "mysql2/promise";
+
+const dbConfig = {
+  host: "omnitools.cluster-cnc10a6gbp2t.us-east-2.rds.amazonaws.com",
+  port: "3306",
+  user: "admin",
+  password: "LinxOmniTools2023",
+  database: "omnitools",
+  connectionLimit: 10,
+  queueLimit: 0,
+};
+
+export async function getConnection() {
+  const connection = await mysql.createConnection(dbConfig);
+  return connection;
+}
